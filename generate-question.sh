7d54c4225581cea64378fcd1a5dd08730adcc3ba
Adam Saponara <saponara@gmail.com>
2016-04-20 23:30:23 -0400

    Add comments

--- generate-question.sh
+++ generate-question.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 this_dir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+
+# Check for output argument
 output_fname=$1
 if [ -z $output_fname ]; then
     echo "Usage: ./generate-question.sh <output_fname>"
     exit 1
 fi
 
+# Run question through text-to-speech
 question=$(shuf -n1 "$this_dir/questions")
 question="Question <break strength='strong'/>
     <prosody rate='.85'>$question</prosody>
