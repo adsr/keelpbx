82e20bc27dbef6726a6d23d352c219952ce1be41
Adam Saponara <as@php.net>
2019-05-12 18:30:39 -0400

    Update repeater

--- generate-repeater.sh
+++ generate-repeater.sh
@@ -11,8 +11,8 @@ fi
 
 # Get random incoming call not from caller
 # Only look at `$last_n` recordings
-last_n=60
-speaker_fname=$(ls -1t "$this_dir/recordings/" | grep '\.wav$' | grep -v $phone_num | grep -Pv '(playback|tmp|out)' | head -n $last_n | shuf -n1)
+last_n=30
+speaker_fname=$(ls -1t "$this_dir/recordings/" | grep '\-in\.wav$' | grep -v $phone_num | grep -Pv '(playback|tmp|out)' | head -n $last_n | shuf -n1)
 speaker_fname="$this_dir/recordings/$speaker_fname"
 
 # Copy to output_fname
